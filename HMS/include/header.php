85ab4768d48856f1effbcc59f214d7a15513f1c0
Saurabh Kumar Singh <77530794+sksusha8853@users.noreply.github.com>
2021-10-19 13:06:27 +0530

    commit

---
+++ HMS/include/header.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+
+    <title>Document</title>
+    <script src="https://code.jquery.com/jquery-3.6.0.slim.js" integrity="sha256-HwWONEZrpuoh951cQD1ov2HUK5zA5DwJ1DNUXaM6FsY=" crossorigin="anonymous"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+
+    <link rel="stylesheet" href="static\style.css" />
+
+</head>
+
+<body style="background-image: url(image/bg.jpg); background-repeat: no-repeat; background-size:cover;">
+
+<div class="upper-container">
+
+        <div class="hms">
+          Hospital Management System
+
+        </div>
+        <div class="links">
+          <?php
+          if(isset($_SESSION['admin'])){
+            $user =$_SESSION['admin'];
+            echo '
+            
+          <a href="" class="link-doctor" >'.$user.'</a>
+          <a href="../admin/logout.php" class="link-patient">Logout</a>
+            
+            ';
+
+          }
+          else{
+            echo '
+            <a href="adminlogin.php" class="link-admin">Admin</a>
+          <a href="" class="link-doctor" >Doctor</a>
+          <a href="" class="link-patient">Patient</a>
+            ';
+
+          }
+          ?>
+          
+        </div>
+
+
+    </div>
+
+
+</body>
+</html>
